96a3fd1c9433aebddc75ef716bbac8fff50d0646
Jasper Yu <007seadog@gmail.com>
2023-05-08 12:46:01 +0800

    Not made by me

---
+++ Js/events.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     $("header").load("menu-bar.html");
+//     $(".contact").load("contact-bar.html");
+//     $(".content .event .item").hover(function() {
+//         $(this).slideDown(2000);
+//     })
+// })
