8173a74d03762b1adb4d89b4e9f7a73a0f3ec764
Jasper Yu <007seadog@gmail.com>
2023-09-03 20:43:01 +0800

    Update to v2

--- Js/events.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//     $("header").load("menu-bar.html");
-//     $(".contact").load("contact-bar.html");
-//     $(".content .event .item").hover(function() {
-//         $(this).slideDown(2000);
-//     })
-// })
