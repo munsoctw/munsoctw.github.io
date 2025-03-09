36c9ce3ebd7e6a6f4fa5939576647ba38b5a3f8c
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-03-09 16:48:08 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ function App() {
 
   const getBGcolor = () => {
     switch (location.pathname) {
-      case '/MUN-Society-Taiwan/':
-        return "var(--dark-blue-bg)"
+      case '/munsoc.tw/':
+        // return "var(--dark-blue-bg)"
+        return "#fff"
       case '/MUN-Society-Taiwan/about':
         return "#ffffff"
       case '/MUN-Society-Taiwan/events':
