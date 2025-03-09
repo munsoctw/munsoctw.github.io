c327d3d88958ab7502881731b33e249caa296623
Justin Pan <justinpanwithoutjustin195@gmail.com>
2025-03-09 16:41:32 +0800

    Add files via upload

---
+++ src/components/MainCover.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const MainCover = () => {
+  return (
+    <>
+      <h1>MUN SOCIETY TAIWAN</h1>
+      <p>Not in the UN, MUNing anywhere.</p>
+    </>
+  )
+}
+
+export default MainCover
