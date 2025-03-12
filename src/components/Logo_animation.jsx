f3d4c9eee83bff6ecbbb4a2f745e3fcb183da1b3
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-03-13 01:58:18 +0800

    Logo_Animation Update

--- src/components/Logo_animation.jsx
+++ src/components/Logo_animation.jsx
@@ -5,9 +5,9 @@ import '../css/explore_btn.css'
 
 const Logo_animation = () => {
   return (
-    <div className="Logo_Animation" style={{marginTop:"-150px"}}>
+    <div className="Logo_Animation">
           <div className="Logo_Container">
-            <video className="Logo_video" src="MST_Logo_animation.mp4" autoPlay muted playsInline>
+            <video className="width-100" src="MST_Logo_animation.mp4" autoPlay muted playsInline>
               {/* <source src={mstIcon.logo_gif} type='video/mp4' /> */}
               {/* <source src="public\MST_Logo_animation.mp4" type='video/mp4' /> */}
             </video>
