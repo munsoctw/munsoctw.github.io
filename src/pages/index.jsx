edc761f07ad80bdfeea1f2bf622bd2cb140c0676
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-03-13 23:51:25 +0800

    Updates

--- src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-export {default as Home} from "./Home";
-export {default as About} from "./About";
-export {default as News} from "./News";
-export {default as Events} from "./Events";
-export {default as Conferences} from "./Conferences";
-export {default as Contact} from "./Contact";
