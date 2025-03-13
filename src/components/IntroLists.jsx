0497d0de19660ffc2ae3d8f0219fe45d32304eac
JustinPanCasetoMe <justinpanwithoutjustin195@gmail.com>
2025-03-13 11:25:46 +0800

    About page introPage Update

--- src/components/IntroLists.jsx
+++ src/components/IntroLists.jsx
@@ -10,13 +10,23 @@ const IntroLists = ( {listings} ) => {
   
     const renderParagraph = item.content.map((paragraph, index) => {
       return( 
-        <p className="paragraph PageXContainer" key={index}>{paragraph}</p>
+        <p className="mg-b-10" key={index}>{paragraph}</p>
       )
     })
 
     return (
-      <div key={index} className="intro" id={item.id}>
-        <h1 className="topic">{item.topic}</h1>
+      <div key={index} className="intro PageXContainer" id={item.id}>
+        <h1
+          style={{
+            fontSize: "36px",
+            fontWeight: "600",
+            borderBottom: "1px solid rgb(161, 161, 161)",
+            marginBottom: "20px",
+            paddingBottom: "12px"
+          }}
+        >
+          {item.topic}
+        </h1>
         {renderParagraph}
       </div>
     )
